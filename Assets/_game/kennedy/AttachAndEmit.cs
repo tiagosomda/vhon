9e8c00d719025d26eafec1568fef57b450a2438f
Kennedy Brown <kennedy.c.brown@gmail.com>
2017-08-05 13:09:52 -0700

    AttachAndEmit script; PowerCubeAttach prefab

---
+++ Assets/_game/kennedy/AttachAndEmit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using NewtonVR;
+
+public class AttachAndEmit : NVRAttachJoint
+{
+    // Time after attach but before activate
+    public float attachTimeout = .25f;
+    // ---
+    public UnityEvent activatees;
+
+
+    // Stores coroutine for possible stopping
+    private Coroutine _activate;
+
+
+    protected override void Attach(NVRAttachPoint point)
+    {
+        base.Attach(point);
+        _activate = StartCoroutine(ActivateThings());
+    }
+
+    protected override void Detach()
+    {
+        if (_activate != null)
+        {
+            StopCoroutine(_activate);
+        }
+    }
+
+    private IEnumerator ActivateThings()
+    {
+        yield return new WaitForSeconds(attachTimeout);
+        activatees.Invoke();
+    }
+}
