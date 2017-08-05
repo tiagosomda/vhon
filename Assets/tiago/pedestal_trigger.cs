c0b2faad81359b6c6dc14504857a80edbac1ad41
tiagosomda <tsomda@gmail.com>
2017-08-05 01:20:51 -0700

    added some random stuff

--- Assets/tiago/pedestal_trigger.cs
+++ Assets/tiago/pedestal_trigger.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pedestal_trigger : MonoBehaviour {
+public class pedestal_trigger : Singleton<pedestal_trigger> {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
