7e61cd2d16cb948925867d7cc30de9a30fa80416
Kennedy Brown <kennedy.c.brown@gmail.com>
2017-08-06 16:34:08 -0700

    room manager

---
+++ Assets/_game/kennedy/Scene Manager/SceneManagerBehavior.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerBehavior : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private float timer;
+
+
+    public AudioClip introClip;
+    public float introWait;
+    private bool cpIntroClip;
+    private float introClipTime;
+
+
+    public Camera fadeInCamera;
+    public float fadeInDuration;
+    public float fadeInWait;
+    private bool cpFadeIn;
+    private float fadeInTime;
+
+
+    public AudioClip instructionClip;
+    public float instructionWait;
+    private bool cpIntruction;
+    private float instructionTime;
+
+
+    public GameObject controller;
+    public float unlockControlsWait;
+    private bool cpUnlockControls;
+    private float unlockControlsTime;
+
+
+//    public AudioClip waitingRoomClip; // reset timer when kicking off this clip
+//    public float openDoorTime;
+
+
+    void Start()
+    {
+        introClipTime = Mathf.Max(introWait, 0.0001f);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (!cpIntroClip && introClipTime != 0 && timer > introClipTime)
+        {
+            cpIntroClip = true;
+
+            audioSource.PlayOneShot(introClip);
+            Debug.Log("played introClip");
+
+            fadeInTime = timer + introClip.length + fadeInWait;
+            return;
+        }
+
+        else if (!cpFadeIn && fadeInTime != 0 && timer > fadeInTime)
+        {
+            cpFadeIn = true;
+
+            StartCoroutine(FadeIn());
+            Debug.Log("started FadeIn");
+
+            instructionTime = timer + fadeInDuration + instructionWait;
+            return;
+        }
+
+        else if (!cpIntruction && instructionTime != 0 && timer > instructionTime)
+        {
+            cpIntruction = true;
+
+            audioSource.PlayOneShot(instructionClip);
+            Debug.Log("played instructionClip");
+
+            unlockControlsTime = timer + instructionClip.length + unlockControlsWait;
+        }
+
+        else if (!cpUnlockControls && unlockControlsTime != 0 && timer > unlockControlsTime)
+        {
+            cpUnlockControls = true;
+
+            controller.SetActive(true);
+            Debug.Log("Unlocked controls");
+        }
+    }
+
+
+    private IEnumerator FadeIn()
+    {
+        // TODO: implement me
+        //   * use "fadeInCamera" and "fadeInDuration"
+        yield return new WaitForEndOfFrame();
+    }
+	
+}
