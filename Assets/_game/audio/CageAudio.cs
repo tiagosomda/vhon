0f6cb9181fc946b291ef3a9b76068638ad9482d9
Simon <simons007@gmail.com>
2017-08-06 11:53:58 -0700

    CageAudio

---
+++ Assets/_game/audio/CageAudio.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageAudio : MonoBehaviour {
+
+    private static CageAudio _instance;
+    public static CageAudio Instance { get { return _instance; } }
+
+    public AudioClip OpenCage;
+    public AudioClip CloseCage;
+
+    private AudioSource AudioSource;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void DefaultTrigger(bool state)
+    {
+        if (state)
+        {
+            Open();
+        } else
+        {
+            Close();
+        }
+    }
+
+    public void Open()
+    {
+        AudioSource.Stop();
+        AudioSource.PlayOneShot(OpenCage);
+    }
+    public void Close()
+    {
+        AudioSource.Stop();
+        AudioSource.PlayOneShot(CloseCage);
+    }
+}
