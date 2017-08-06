aae2bba4a15d59f5cebd01dac550bb941290bd02
Tiago <tsomda@gmail.com>
2017-08-06 14:56:01 -0700

    almost converted the whole teleportation

--- Assets/tiago/VHONTeleportation.cs
+++ Assets/tiago/VHONTeleportation.cs
@@ -22,12 +22,28 @@ public class VHONTeleportation : MonoBehaviour {
 
     public Color pointerLockedColor;
     public Color pointerValidColor;
+    public Color pointerInvalidColor;
 
     private Vector3 pointedAtPosition;
-
-    public Transform offsetReticleTransform;
-
+    private Vector3 prevPointedAtPosition;
     private TeleportMarkerBase pointedAtTeleportMarker;
+
+    private float invalidReticleMinScale = 0.2f;
+	private float invalidReticleMaxScale = 1.0f;
+	private float invalidReticleMinScaleDistance = 0.4f;
+	private float invalidReticleMaxScaleDistance = 2.0f;
+	private Vector3 invalidReticleScale = Vector3.one;
+    private Quaternion invalidReticleTargetRotation = Quaternion.identity;
+
+    private bool visible = false;
+
+
+    [Header( "Debug" )]
+	public bool debugFloor = false;
+	public bool showOffsetReticle = false;
+	public Transform offsetReticleTransform;
+	public MeshRenderer floorDebugSphere;
+	public LineRenderer floorDebugLine;
 
     private void Awake()
     {
@@ -59,11 +75,12 @@ public class VHONTeleportation : MonoBehaviour {
         Line.enabled = (Hand != null && Hand.Inputs[NVRButtons.Touchpad].IsPressed);
         if (Line.enabled == true)
         {
-            NvrTeleport();
-            //VHONTeleport();
+            //NvrTeleport();
+            VHONTeleport();
         }
         else {
-            teleportArc.Hide();
+            //teleportArc.Hide();
+            HidePointer();
         }
     }
 
@@ -132,6 +149,7 @@ public class VHONTeleportation : MonoBehaviour {
 		if ( teleportArc.DrawArc( out hitInfo ) )
 		{
 			hitSomething = true;
+            hitTeleportMarker = hitInfo.collider.GetComponentInParent<TeleportMarkerBase>();
 		}
 
         if ( pointerAtBadAngle )
@@ -139,7 +157,7 @@ public class VHONTeleportation : MonoBehaviour {
 			hitTeleportMarker = null;
 		}
 
-		//HighlightSelected( hitTeleportMarker );
+		HighlightSelected( hitTeleportMarker );
 
 		if ( hitTeleportMarker != null ) //Hit a teleport marker
 		{
@@ -164,6 +182,7 @@ public class VHONTeleportation : MonoBehaviour {
 				Line.endColor = pointerValidColor; //pointerLineRenderer.endColor = pointerValidColor;
                 #endif
 				destinationReticleTransform.gameObject.SetActive( hitTeleportMarker.showReticle );
+                destinationReticleTransform.rotation = Quaternion.LookRotation(Vector3.forward); //Quaternion. .Slerp( invalidReticleTransform.rotation, invalidReticleTargetRotation, 0.1f );
 			}
 
 			offsetReticleTransform.gameObject.SetActive( true );
@@ -172,58 +191,198 @@ public class VHONTeleportation : MonoBehaviour {
 
 			pointedAtTeleportMarker = hitTeleportMarker;
 			pointedAtPosition = hitInfo.point;
+
+            /*
+            if ( showPlayAreaMarker )
+			{
+				//Show the play area marker if this is a teleport area
+				TeleportArea teleportArea = pointedAtTeleportMarker as TeleportArea;
+				if ( teleportArea != null && !teleportArea.locked && playAreaPreviewTransform != null )
+				{
+					Vector3 offsetToUse = playerFeetOffset;
+					//Adjust the actual offset to prevent the play area marker from moving too much
+					if ( !movedFeetFarEnough )
+					{
+						float distanceFromStartingOffset = Vector3.Distance( playerFeetOffset, startingFeetOffset );
+						if ( distanceFromStartingOffset < 0.1f )
+						{
+							offsetToUse = startingFeetOffset;
+						}
+						else if ( distanceFromStartingOffset < 0.4f )
+						{
+							offsetToUse = Vector3.Lerp( startingFeetOffset, playerFeetOffset, ( distanceFromStartingOffset - 0.1f ) / 0.3f );
+						}
+						else
+						{
+							movedFeetFarEnough = true;
+						}
+					}
+
+					playAreaPreviewTransform.position = pointedAtPosition + offsetToUse;
+						showPlayAreaPreview = true;
+				}
+			}
+            */
+
+            pointerEnd = hitInfo.point;
         }
-        /*
         else //Hit neither
 		{
-				destinationReticleTransform.gameObject.SetActive( false );
-				offsetReticleTransform.gameObject.SetActive( false );
+			destinationReticleTransform.gameObject.SetActive( false );
+			offsetReticleTransform.gameObject.SetActive( false );
 
-				teleportArc.SetColor( pointerInvalidColor );
-#if (UNITY_5_4)
-				pointerLineRenderer.SetColors( pointerInvalidColor, pointerInvalidColor );
-#else
-				pointerLineRenderer.startColor = pointerInvalidColor;
-				pointerLineRenderer.endColor = pointerInvalidColor;
-#endif
-				invalidReticleTransform.gameObject.SetActive( !pointerAtBadAngle );
+			teleportArc.SetColor( pointerInvalidColor );
+            #if (UNITY_5_4)
+			Line.SetColors( pointerInvalidColor, pointerInvalidColor );//pointerLineRenderer.SetColors( pointerInvalidColor, pointerInvalidColor );
+            #else
+			Line.startColor = pointerInvalidColor; //pointerLineRenderer.startColor = pointerInvalidColor;
+			Line.endColor = pointerInvalidColor; //pointerLineRenderer.endColor = pointerInvalidColor;
+            #endif
+			invalidReticleTransform.gameObject.SetActive( !pointerAtBadAngle );
 
-				//Orient the invalid reticle to the normal of the trace hit point
-				Vector3 normalToUse = hitInfo.normal;
-				float angle = Vector3.Angle( hitInfo.normal, Vector3.up );
-				if ( angle < 15.0f )
+			//Orient the invalid reticle to the normal of the trace hit point
+			Vector3 normalToUse = hitInfo.normal;
+			float angle = Vector3.Angle( hitInfo.normal, Vector3.up );
+			if ( angle < 15.0f )
+			{
+				normalToUse = Vector3.up;
+			}
+			invalidReticleTargetRotation = Quaternion.FromToRotation( Vector3.up, normalToUse );
+			invalidReticleTransform.rotation = Quaternion.Slerp( invalidReticleTransform.rotation, invalidReticleTargetRotation, 0.1f );
+
+			//Scale the invalid reticle based on the distance from the player
+			float distanceFromPlayer = Vector3.Distance( hitInfo.point, Player.transform.position );// Vector3.Distance( hitInfo.point, player.hmdTransform.position );
+			float invalidReticleCurrentScale = Util.RemapNumberClamped( distanceFromPlayer, invalidReticleMinScaleDistance, invalidReticleMaxScaleDistance, invalidReticleMinScale, invalidReticleMaxScale );
+			invalidReticleScale.x = invalidReticleCurrentScale;
+			invalidReticleScale.y = invalidReticleCurrentScale;
+			invalidReticleScale.z = invalidReticleCurrentScale;
+			invalidReticleTransform.transform.localScale = invalidReticleScale;
+
+			pointedAtTeleportMarker = null;
+
+			if ( hitSomething )
+			{
+				pointerEnd = hitInfo.point;
+			}
+			else
+			{
+				pointerEnd = teleportArc.GetArcPositionAtTime( teleportArc.arcDuration );
+			}
+
+			//Debug floor
+			if ( debugFloor )
+			{
+				floorDebugSphere.gameObject.SetActive( false );
+				floorDebugLine.gameObject.SetActive( false );
+			}
+		}
+
+        //if ( playAreaPreviewTransform != null )
+		//{
+		//	playAreaPreviewTransform.gameObject.SetActive( showPlayAreaPreview );
+		//}
+
+		if ( !showOffsetReticle )
+		{
+			offsetReticleTransform.gameObject.SetActive( false );
+		}
+		destinationReticleTransform.position = pointedAtPosition;
+		invalidReticleTransform.position = pointerEnd;
+		//onActivateObjectTransform.position = pointerEnd;
+		//onDeactivateObjectTransform.position = pointerEnd;
+		offsetReticleTransform.position = pointerEnd - playerFeetOffset;
+		//reticleAudioSource.transform.position = pointedAtPosition;
+		Line.SetPosition( 0, pointerStart ); //pointerLineRenderer.SetPosition( 0, pointerStart );
+		Line.SetPosition( 1, pointerEnd ); //pointerLineRenderer.SetPosition( 1, pointerEnd );
+    }         
+
+    private void HighlightSelected(TeleportMarkerBase hitTeleportMarker )
+	{
+		if ( pointedAtTeleportMarker != hitTeleportMarker ) //Pointing at a new teleport marker
+		{
+			if ( pointedAtTeleportMarker != null )
+			{
+				pointedAtTeleportMarker.Highlight( false );
+			}
+			if ( hitTeleportMarker != null )
+			{
+				hitTeleportMarker.Highlight( true );
+				prevPointedAtPosition = pointedAtPosition;
+				//PlayPointerHaptic( !hitTeleportMarker.locked );
+				//PlayAudioClip( reticleAudioSource, goodHighlightSound );
+				//loopingAudioSource.volume = loopingAudioMaxVolume;
+			}
+			else if ( pointedAtTeleportMarker != null )
+			{
+				//PlayAudioClip( reticleAudioSource, badHighlightSound );
+				//loopingAudioSource.volume = 0.0f;
+			}
+		}
+		else if ( hitTeleportMarker != null ) //Pointing at the same teleport marker
+		{
+			if ( Vector3.Distance( prevPointedAtPosition, pointedAtPosition ) > 1.0f )
+			{
+				prevPointedAtPosition = pointedAtPosition;
+				//PlayPointerHaptic( !hitTeleportMarker.locked );
+			}
+		}
+	}
+
+
+    private void HidePointer()
+	{
+		if ( visible )
+		{
+			//pointerHideStartTime = Time.time;
+		}
+
+		visible = false;
+		if ( Hand )
+		{
+            /*
+			if ( ShouldOverrideHoverLock() )
+			{
+				//Restore the original hovering interactable on the hand
+				if ( originalHoverLockState == true )
 				{
-					normalToUse = Vector3.up;
-				}
-				invalidReticleTargetRotation = Quaternion.FromToRotation( Vector3.up, normalToUse );
-				invalidReticleTransform.rotation = Quaternion.Slerp( invalidReticleTransform.rotation, invalidReticleTargetRotation, 0.1f );
-
-				//Scale the invalid reticle based on the distance from the player
-				float distanceFromPlayer = Vector3.Distance( hitInfo.point, player.hmdTransform.position );
-				float invalidReticleCurrentScale = Util.RemapNumberClamped( distanceFromPlayer, invalidReticleMinScaleDistance, invalidReticleMaxScaleDistance, invalidReticleMinScale, invalidReticleMaxScale );
-				invalidReticleScale.x = invalidReticleCurrentScale;
-				invalidReticleScale.y = invalidReticleCurrentScale;
-				invalidReticleScale.z = invalidReticleCurrentScale;
-				invalidReticleTransform.transform.localScale = invalidReticleScale;
-
-				pointedAtTeleportMarker = null;
-
-				if ( hitSomething )
-				{
-					pointerEnd = hitInfo.point;
+					pointerHand.HoverLock( originalHoveringInteractable );
 				}
 				else
 				{
-					pointerEnd = teleportArc.GetArcPositionAtTime( teleportArc.arcDuration );
+					pointerHand.HoverUnlock( null );
 				}
+			}
+            */
 
-				//Debug floor
-				if ( debugFloor )
-				{
-					floorDebugSphere.gameObject.SetActive( false );
-					floorDebugLine.gameObject.SetActive( false );
-				}
+			//Stop looping sound
+			//loopingAudioSource.Stop();
+			//PlayAudioClip( pointerAudioSource, pointerStopSound );
+		}
+		
+        //teleportPointerObject.SetActive( false );
+
+		teleportArc.Hide();
+		/*
+        foreach ( TeleportMarkerBase teleportMarker in teleportMarkers )
+		{
+			if ( teleportMarker != null && teleportMarker.markerActive && teleportMarker.gameObject != null )
+			{
+				teleportMarker.gameObject.SetActive( false );
+			}
 		}
         */
-    }         
+		destinationReticleTransform.gameObject.SetActive( false );
+		invalidReticleTransform.gameObject.SetActive( false );
+		offsetReticleTransform.gameObject.SetActive( false );
+		//if ( playAreaPreviewTransform != null )
+		//{
+		//	playAreaPreviewTransform.gameObject.SetActive( false );
+		//}
+		//if ( onActivateObjectTransform.gameObject.activeSelf )
+		//{
+		//	onActivateObjectTransform.gameObject.SetActive( false );
+		//}
+		//onDeactivateObjectTransform.gameObject.SetActive( true );
+		//pointerHand = null;
+	}
 }
